2c52791146087235cb8e21067c3273a28166844a
Waz kuruni <woazkurunijoy@gmail.com>
2026-09-26 02:59:27 +0600

    Add production error logging

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -352,3 +352,31 @@ CSRF_COOKIE_SECURE = not DEBUG
 # =========================================================
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# =========================================================
+# LOGGING
+# =========================================================
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": "INFO",
+        },
+
+        "django.request": {
+            "handlers": ["console"],
+            "level": "ERROR",
+            "propagate": False,
+        },
+    },
+}
